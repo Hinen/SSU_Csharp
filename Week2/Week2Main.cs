66cb26a90f5c2480edae9563b9479c6b8a272f2e
Hinen <qkrqldtjqj@naver.com>
2021-03-25 00:24:10 +0900

    Create Proj

---
+++ Week2/Week2Main.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SoongSil_University_Csharp {
+    class Week2Main {
+        static void Main(string[] args) {
+            Console.Out.WriteLine("Hello");
+        }
+    }
+}
