bbfe772ff14b647a8605be6a54f303cd3423a2ea
Hinen <qkrqldtjqj@naver.com>
2021-04-01 21:07:54 +0900

    Type Change Test

--- Week3/Week3Manager.cs
+++ Week3/Week3Manager.cs
@@ -1,11 +1,25 @@
-﻿using SoongSil_University_Csharp.Core;
+﻿using System;
+using SoongSil_University_Csharp.Core;
 
 namespace SoongSil_University_Csharp.Week3 {
     class Week3Manager : WeekManagerBase {
         public Week3Manager() : base(3) { }
 
         public override void Main() {
+            TypeChangeTest();
+        }
 
+        private void TypeChangeTest() {
+            int num = 12345;
+            string str = "123";
+
+            string numToStr = num.ToString();
+            int strToNum = int.Parse(str);
+
+            Console.Out.WriteLine($"num : {num}, {num.GetType()}");
+            Console.Out.WriteLine($"str : {str}, {str.GetType()}");
+            Console.Out.WriteLine($"numToStr : {numToStr}, {numToStr.GetType()}");
+            Console.Out.WriteLine($"strToNum : {strToNum}, {strToNum.GetType()}");
         }
     }
 }
