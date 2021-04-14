62c11e17b3f49e0bc80c2c92f474ace90719c3cc
Hinen <qkrqldtjqj@naver.com>
2021-04-14 23:43:54 +0900

    out test

--- Week5/Week5Manager.cs
+++ Week5/Week5Manager.cs
@@ -6,7 +6,8 @@ namespace SoongSil_University_Csharp.Week5 {
         public Week5Manager() : base(5) { }
 
         public override void Main() {
-            refTest(); 
+            //refTest(); 
+            outTest();
         }
 
         private void refTest() {
@@ -18,7 +19,23 @@ namespace SoongSil_University_Csharp.Week5 {
         }
 
         private void refTestImpl(ref int num) {
+            // ref의 경우 넘겨주는 매개변수 초기화가 반드시 필요
             num = 10;
+        }
+
+        private void outTest() {
+            int num = 10;
+            int result = 5;
+            Console.WriteLine("out result : " + result);
+
+            outTestImpl(num, out result);
+            Console.WriteLine("out result : " + result);
+        }
+
+        private void outTestImpl(int num, out int result) {
+            // out의 경우 넘겨주는 매개변수 초기화가 필요 없음
+            // out에서는 넘겨주는 매개변수 초기 값을 무시하기 때문
+            result = num * num;
         }
     }
 }
