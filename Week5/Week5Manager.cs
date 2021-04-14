38b2bc651d856bb4cade4697636e981a76492458
Hinen <qkrqldtjqj@naver.com>
2021-04-14 23:40:19 +0900

    Create Week5Manager and refTest

---
+++ Week5/Week5Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SoongSil_University_Csharp.Core;
+
+namespace SoongSil_University_Csharp.Week5 {
+    class Week5Manager : WeekManagerBase {
+        public Week5Manager() : base(5) { }
+
+        public override void Main() {
+            refTest(); 
+        }
+
+        private void refTest() {
+            int num = 1;
+            Console.WriteLine("ref num : " + num);
+
+            refTestImpl(ref num);
+            Console.WriteLine("ref num : " + num);
+        }
+
+        private void refTestImpl(ref int num) {
+            num = 10;
+        }
+    }
+}
