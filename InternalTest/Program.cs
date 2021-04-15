6a294a8ccacfe083ac0ae4942e66ee634ba8526b
Hinen <qkrqldtjqj@naver.com>
2021-04-16 01:13:42 +0900

    Internal, protected internal

---
+++ InternalTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InternalTest {
+    public class InternalTestClass {
+        // internal은 같은 어셈블리 내에서 public과 동일함
+        internal virtual void TestInternalFunc() {
+            Console.WriteLine("Internal Function");
+        }
+
+        /*
+         * protected internal은 같은 어셈블리 내에서는 internal과 동일
+         * 단, 외부 어셈블리에서 해당 클래스를 상속 받아서 사용할 경우엔
+         * internal은 여전히 접근이 불가하나, 
+         * protected internal은 상속한 해당 클래스 내부에서는 접근이 가능함
+        */
+        protected internal void TestProtectedInternalFunc() {
+            Console.WriteLine("Protected Internal Function");
+        }
+
+        public virtual void PublicTestFunc() {
+            Console.WriteLine("Public Function");
+        }
+
+        protected void ProtectedTestFunc() {
+            Console.WriteLine("Protected Function");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var test1 = new InternalTestClass();
+
+            test1.TestInternalFunc();
+            test1.TestProtectedInternalFunc();
+            test1.PublicTestFunc();
+            //test1.ProtectedTestFunc();
+        }
+    }
+}
--- Week6/Week6Manager.cs
+++ Week6/Week6Manager.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using SoongSil_University_Csharp.Core;
+﻿using SoongSil_University_Csharp.Core;
+using InternalTest;
 
 namespace SoongSil_University_Csharp.Week6 {
     class Week6Manager : WeekManagerBase {
         public Week6Manager() : base(6) { }
 
         public override void Main() {
-            
+            InternalTest();
+        }
+
+        private void InternalTest() {
+            var test1 = new InternalTestClass();
+
+            /*
+             * can not access,
+             * can access only public 
+             */
+            //test1.TestInternalFunc(); 
+            test1.PublicTestFunc();
+
+            var test3 = new ProtectedInternalTestClass();
+            test3.testFunction();
+        }
+    }
+
+    class ProtectedInternalTestClass : InternalTestClass {
+        public void testFunction() {
+            // can access protected internal only in class
+            TestProtectedInternalFunc();
         }
     }
 }
