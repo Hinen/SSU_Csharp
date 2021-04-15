4f1d40374e0784c66dca0c8d31192535a7790666
Hinen <qkrqldtjqj@naver.com>
2021-04-16 00:11:57 +0900

    Create Week6Manager

---
+++ Week6/Week6Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SoongSil_University_Csharp.Core;
+
+namespace SoongSil_University_Csharp.Week6 {
+    class Week6Manager : WeekManagerBase {
+        public Week6Manager() : base(6) { }
+
+        public override void Main() {
+            
+        }
+    }
+}
