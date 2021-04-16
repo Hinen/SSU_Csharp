c87bb0a8164bb12e784e6739865e1af91286e150
Hinen <qkrqldtjqj@naver.com>
2021-04-17 00:51:04 +0900

    Create Week7Manager

---
+++ Week7/Week7Manager.cs
@@ -0,0 +1,11 @@
+﻿using SoongSil_University_Csharp.Core;
+
+namespace SoongSil_University_Csharp.Week7 {
+    class Week7Manager : WeekManagerBase {
+        public Week7Manager() : base(7) { }
+
+        public override void Main() {
+
+        }
+    }
+}
