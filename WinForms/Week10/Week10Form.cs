3a7e5bd74d44718a3b943ca22cfbafdf1476e0ad
Hinen <qkrqldtjqj@naver.com>
2021-06-06 02:36:14 +0900

    Process.Start

--- WinForms/Week10/Week10Form.cs
+++ WinForms/Week10/Week10Form.cs
@@ -43,11 +43,31 @@ namespace WinForms.Week10 {
         }
 
         private void naverLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
-            Process.Start(naverLink.Text);
+            // 단순히 이렇게 실행도 가능하나, 최신 브라우저에서는 보안상 실행이 불가능
+            //Process.Start(naverLink.Text);
+
+            string url = naverLink.Text;
+
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+
+            // 기본 익스플로러가 제대로 설정 되지 않으면, Process Start을 실행 못 시킬 수 있음
+            Process.Start(psi);
         }
 
         private void youtubeLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
-            Process.Start(youtubeLink.Text);
+            // 단순히 이렇게 실행도 가능하나, 최신 브라우저에서는 보안상 실행이 불가능
+            //Process.Start(youtubeLink.Text);
+
+            string url = youtubeLink.Text;
+
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+
+            // 기본 익스플로러가 제대로 설정 되지 않으면, Process Start을 실행 못 시킬 수 있음
+            Process.Start(psi);
         }
     }
 }
