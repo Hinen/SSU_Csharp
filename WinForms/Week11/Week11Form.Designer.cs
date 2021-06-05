a6ad8901de9093034bac0fe6bba377e07f91eff7
Hinen <qkrqldtjqj@naver.com>
2021-06-06 03:29:25 +0900

    CheckBox

--- WinForms/Week11/Week11Form.Designer.cs
+++ WinForms/Week11/Week11Form.Designer.cs
@@ -23,19 +23,75 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBoxLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 34);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 56);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBoxLabel
+            // 
+            this.checkBoxLabel.AutoSize = true;
+            this.checkBoxLabel.Location = new System.Drawing.Point(10, 85);
+            this.checkBoxLabel.Name = "checkBoxLabel";
+            this.checkBoxLabel.Size = new System.Drawing.Size(92, 12);
+            this.checkBoxLabel.TabIndex = 3;
+            this.checkBoxLabel.Text = "checkBoxLabel";
             // 
             // Week11Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(575, 538);
+            this.Controls.Add(this.checkBoxLabel);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Name = "Week11Form";
             this.Text = "Week11Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label checkBoxLabel;
     }
 }
--- WinForms/Week11/Week11Form.cs
+++ WinForms/Week11/Week11Form.cs
@@ -13,5 +13,29 @@ namespace WinForms.Week11 {
         public Week11Form() {
             InitializeComponent();
         }
+
+        private void RefreshCheckBoxLabel(CheckBox checkBox) {
+            var text = checkBox.Text;
+
+            if (checkBox.Checked)
+                checkBoxLabel.Text += " " + text;
+            else {
+                var idx = checkBoxLabel.Text.IndexOf(text) - 1;
+
+                checkBoxLabel.Text = checkBoxLabel.Text.Remove(idx, text.Length + 1);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            RefreshCheckBoxLabel(checkBox1);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) {
+            RefreshCheckBoxLabel(checkBox2);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) {
+            RefreshCheckBoxLabel(checkBox3);
+        }
     }
 }
