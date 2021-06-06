7719fbb828782bc813184bf27560aed16530d6c1
Hinen <qkrqldtjqj@naver.com>
2021-06-06 16:55:14 +0900

    Add Week13 Form

---
+++ WinForms/Week13/Week13Form.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace WinForms.Week13 {
+    partial class Week13Form {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // Week13Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 557);
+            this.Name = "Week13Form";
+            this.Text = "Week13Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WinForms/Week13/Week13Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.Week13 {
+    public partial class Week13Form : Form {
+        public Week13Form() {
+            InitializeComponent();
+        }
+    }
+}
