35b1cde9463c939583e7a6f181b7df584d33a075
Hinen <qkrqldtjqj@naver.com>
2021-06-06 20:26:47 +0900

    thread test

--- Core/ProgramMain.cs
+++ Core/ProgramMain.cs
@@ -6,7 +6,8 @@
             //weekManager = new Week2.Week2Manager();
             //weekManager = new Week3.Week3Manager();
             //weekManager = new Week5.Week5Manager();
-            weekManager = new Week6.Week6Manager();
+            //weekManager = new Week6.Week6Manager();
+            weekManager = new Week14.Week14Manager();
 
             weekManager.Main();
         }
--- Week14/Week14Manager.cs
+++ Week14/Week14Manager.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Threading;
 using SoongSil_University_Csharp.Core;
 
 namespace SoongSil_University_Csharp.Week14 {
     class Week14Manager : WeekManagerBase {
+        private int resource = 0;
+
         public Week14Manager() : base(2) { }
 
         public override void Main() {
-            
+            ThreadTest();
+        }
+
+        private void ThreadTest() {
+            var thread = new Thread(Thread1);
+            thread.Start();
+
+            var thread2 = new Thread(Thread2);
+            thread2.Start();
+
+            thread.Join();
+            thread2.Join();
+        }
+
+        private void Thread1() {
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine(i + resource);
+            }
+        }
+
+        private void Thread2() {
+            resource += 100;
         }
     }
 }
