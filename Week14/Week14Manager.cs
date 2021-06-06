1a963839a501391c8303f369fa65e8210e689656
Hinen <qkrqldtjqj@naver.com>
2021-06-06 20:00:53 +0900

    Add Week14Manager

---
+++ Week14/Week14Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SoongSil_University_Csharp.Core;
+
+namespace SoongSil_University_Csharp.Week14 {
+    class Week14Manager : WeekManagerBase {
+        public Week14Manager() : base(2) { }
+
+        public override void Main() {
+            
+        }
+    }
+}
